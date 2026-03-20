bc4c48e6c30382052adbb2b7275b62876d49e0b7
Caleb Ventura <calebventura845@gmail.com>
2026-03-19 22:20:27 -0400

    Restore Fieldr element-focused motion

--- fieldr-landing/src/App.jsx
+++ fieldr-landing/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AmbientBackground } from './components/AmbientBackground'
 import { Nav } from './components/Nav'
 import Home from './pages/Home'
 import HowItWorks from './pages/HowItWorks'
@@ -9,17 +8,14 @@ import BookDemo from './pages/BookDemo'
 export default function App() {
   return (
     <BrowserRouter>
-      <div style={{ position: 'relative', minHeight: '100vh', isolation: 'isolate', background: 'var(--bg)', color: 'var(--body)', overflowX: 'clip' }}>
-        <AmbientBackground />
-        <div style={{ position: 'relative', zIndex: 1 }}>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/how-it-works" element={<HowItWorks />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/book-demo" element={<BookDemo />} />
-          </Routes>
-        </div>
+      <div style={{ minHeight: '100vh', background: 'var(--bg)', color: 'var(--body)', overflowX: 'clip' }}>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/book-demo" element={<BookDemo />} />
+        </Routes>
       </div>
     </BrowserRouter>
   )
